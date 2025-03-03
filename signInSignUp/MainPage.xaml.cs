2268b0715e97f2ed13e8f8596194267c95e6b2d9
AllenLou4 <84282489+AllenLou4@users.noreply.github.com>
2025-03-04 01:17:48 +0800

    Toggle Password Fix, Deleted AppShell, Added Font, Modify Colors, adjust margins

--- signInSignUp/MainPage.xaml.cs
+++ signInSignUp/MainPage.xaml.cs
@@ -1,10 +1,10 @@
 ﻿using signInSignUp.Pages;
+using signInSignUp.Helpers;
 
 namespace signInSignUp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
         bool isPasswordVisible = false;
 
         public MainPage()
@@ -15,9 +15,7 @@ namespace signInSignUp
 
         private void TogglePass_Clicked(object sender, EventArgs e)
         {
-            isPasswordVisible = !isPasswordVisible;
-            PasswordInput.IsPassword = !isPasswordVisible;
-            TogglePass.Text = isPasswordVisible ? "Hide" : "Show";
+            Utility.TogglePasswordVisibility(PasswordInput, ref isPasswordVisible);
         }
         private void OnSignUpLabelTapped(object sender, EventArgs e)
         {
--- signInSignUp/Pages/SignUp.xaml.cs
+++ signInSignUp/Pages/SignUp.xaml.cs
@@ -1,7 +1,10 @@
 namespace signInSignUp.Pages
 {
+    using signInSignUp.Helpers;
     public partial class SignUpPage : ContentPage
     {
+        bool isPasswordVisible = false;
+
         public SignUpPage()
         {
             InitializeComponent();
@@ -10,12 +13,21 @@ namespace signInSignUp.Pages
 
         private void OnSignUpButtonClicked(object sender, EventArgs e)
         {
-           
         }
 
         private async void OnSignInNav(object sender, TappedEventArgs e)
         {
             await Navigation.PopAsync();
+        }
+
+        private void TogglePass_Clicked(object sender, EventArgs e)
+        {
+            var button = sender as ImageButton;
+            var entry = button?.CommandParameter as Entry;
+            if (entry != null)
+            {
+                Utility.TogglePasswordVisibility(entry, ref isPasswordVisible);
+            }
         }
     }
 }
